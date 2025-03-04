a4b876bf62b3760124eecc8f7c3f4f7b017b1654
Igor Grosu <igrgro@gmail.com>
2025-03-04 17:21:38 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('frontend.urls')),
     *static(settings.STATIC_URL, document_root = settings.STATIC_ROOT),
     *static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT),
     *static(settings.PUBLIC_URL, document_root = settings.PUBLIC_ROOT)
